2f5c37f769df0a3b803ecc53b16fa196f1d38fa2
Bryce Allen <ballen@ci.uchicago.edu>
2012-02-02 14:15:05 -0600

    mkproxy: remove obselete files for using openssl command to sign

--- mkproxy/pipe.sh
+++
@@ -1,1 +0,0 @@
-cat /tmp/usercred.pub | ./mkproxy 2>/dev/null | openssl x509 -md5 -days 1 -CA /tmp/tg.proxy -CAkey /tmp/tg.proxy -extensions v3_proxy -extfile proxy.cnf -CAcreateserial 2>/dev/null | openssl x509 -text -noout
